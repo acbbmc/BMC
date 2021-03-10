0662b7d717736d6c0eae15be5a96b3a0fd6da8e2
acbbmc <anthonycbutcher@gmail.com>
2021-03-10 17:46:07 +0100

    initial commit

---
+++ EmployeeAPI/EmployeeAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployeeAPI.Models;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeesController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+
+        /// <summary>
+        /// Gets list of all Employees, with possibility to filter on name.
+        /// </summary>
+        /// <param name="filters"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees([FromQuery] Filters filters)
+        {
+            var employees = from e in _context.Employees
+                            select e;
+
+            // apply name filtering 
+            if (!string.IsNullOrEmpty(filters.FirstName))
+                employees = employees.Where(e => e.FirstName.ToLowerInvariant().Contains(filters.FirstName.ToLowerInvariant()));
+            
+            if (!string.IsNullOrEmpty(filters.LastName))
+                employees = employees.Where(e => e.LastName.ToLowerInvariant().Contains(filters.LastName.ToLowerInvariant()));
+
+            return await employees.ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets Employee by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(long id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(long id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(long id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(long id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EmployeeAPI/EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
